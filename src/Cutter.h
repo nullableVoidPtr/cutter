5e807f1c84af8338d436c6df46bbaed56e7f85f2
mandlebro <fcasal@users.noreply.github.com>
2018-10-28 13:13:45 +0000

    Fix emulation not working after a debugging session

--- src/Cutter.h
+++ src/Cutter.h
@@ -506,6 +506,7 @@ public:
     void startEmulation();
     void attachDebug(int pid);
     void stopDebug();
+    void syncAndSeekProgramCounter();
     void continueDebug();
     void continueUntilCall();
     void continueUntilSyscall();
